61ea7197770cdb8be2449504c2b0bd3290b62a0f
Wendy Alfaro <166208157+WenYanelly16@users.noreply.github.com>
2025-04-25 23:44:55 +0000

    Added models

--- flashcard-app/models/cardModel.js
+++ flashcard-app/models/cardModel.js
@@ -1,1 +1,33 @@
 //Filename: ./models/cardModel.js
+
+import pool from '../config/db.js';
+
+export const getAllCards = async () => {
+    const result = await pool.query('SELECT * FROM flashcards');
+    return result.rows;
+};
+
+export const getCardById = async (id) => {
+    const result = await pool.query('SELECT * FROM flashcards WHERE id = $1', [id]);
+    return result.rows[0];
+};
+
+export const createCard = async (question, answer) => {
+    const result = await pool.query(
+        'INSERT INTO flashcards (question, answer) VALUES ($1, $2) RETURNING *',
+        [question, answer]
+    );
+    return result.rows[0];
+};
+
+export const updateCard = async (id, question, answer) => {
+    const result = await pool.query(
+        'UPDATE flashcards SET question = $1, answer = $2 WHERE id = $3 RETURNING *',
+        [question, answer, id]
+    );
+    return result.rows[0];
+};
+
+export const deleteCard = async (id) => {
+    await pool.query('DELETE FROM flashcards WHERE id = $1', [id]);
+};
